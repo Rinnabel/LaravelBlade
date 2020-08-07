c9d5ebfcf6111e0d98e772fbbdbc61e65c03854f
Rinnabel <55181896+Rinnabel@users.noreply.github.com>
2020-08-07 12:39:04 +0700

    Tugas Migration Selesai

---
+++ database/migrations/2020_08_07_053157_create_pertanyaan_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePertanyaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pertanyaan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul');
+            $table->longText('isi');
+            $table->unsignedBigInteger('profile_id');
+            $table->unsignedBigInteger('jawaban_tepat_id');
+
+            $table->foreign('profile_id')->references('id')->on('profiles');
+            $table->foreign('jawaban_tepat_id')->references('id')->on('jawaban');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pertanyaan');
+    }
+}
